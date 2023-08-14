f8a103d03faefb2e14a0b4f47640352101b9e4b1
Kayes Ahmed <135023049+KayesInsider@users.noreply.github.com>
2023-08-14 20:27:22 +1000

    Add files via upload

---
+++ 3.3 ShapeDrawing/Program.cs
@@ -0,0 +1,54 @@
+using System;
+using SplashKitSDK;
+
+
+namespace ShapeDrawering
+{
+    public class Program
+    {
+        public static void Main()
+
+        {
+            Window window = new Window("Shape Drawer", 800, 600);
+            Drawing drawing = new Drawing();
+
+            do
+            {
+                SplashKit.ProcessEvents();
+
+                if (SplashKit.MouseClicked(MouseButton.LeftButton))
+                {
+                    Shape myShape = new Shape();
+                    myShape.X = SplashKit.MouseX();
+                    myShape.Y = SplashKit.MouseY();
+                    drawing.AddShape(myShape);
+
+                }
+
+                if (SplashKit.KeyTyped(KeyCode.SpaceKey))
+                {
+                    drawing.Background = SplashKit.RandomRGBColor(255);
+                }
+                if (SplashKit.MouseClicked(MouseButton.RightButton))
+                {
+                    drawing.SelectShapeAt(SplashKit.MousePosition());
+                }
+
+                if (SplashKit.KeyTyped(KeyCode.DeleteKey) || SplashKit.KeyTyped(KeyCode.BackspaceKey))
+                {
+                    foreach (Shape shape in drawing.SelectedShapes)
+                    {
+                        drawing.RemoveShape(shape);
+                    }
+                }
+
+                drawing.Draw();
+                SplashKit.RefreshScreen();
+            }
+
+            while (!window.CloseRequested);
+
+        }
+    }
+}
+
