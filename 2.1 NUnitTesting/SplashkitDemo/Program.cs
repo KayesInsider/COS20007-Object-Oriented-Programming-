aedd6898b9824c6809005cf1bb74683edea6cb82
Kayes Ahmed <135023049+KayesInsider@users.noreply.github.com>
2023-08-14 20:21:30 +1000

    Add files via upload

---
+++ 2.1 NUnitTesting/SplashkitDemo/Program.cs
@@ -0,0 +1,11 @@
+using System;
+using SplashKitSDK;
+
+public class Program
+{
+    public static void Main()
+    {
+        new Window("Test window", 800, 600);
+        SplashKit.Delay(5000);
+    }
+}
